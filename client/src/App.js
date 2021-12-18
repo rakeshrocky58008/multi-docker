ec1234791c842a1265cc565d16d3bc79b4efb8b4
rakesh <rakesh@rakeshs-MacBook-Pro.local>
2021-12-18 19:24:43 +0530

    App.js and .css changed

--- client/src/App.js
+++ client/src/App.js
@@ -10,19 +10,13 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-             Learn React
-          </a>
-          <label>RAKESHS FIB APP</label>
+          <h1 >RAKESH'S FIB APP</h1>
           <Link to="/">Home </Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
+        <section>
+          <label>Welcome to FIB application </label>
+          </section>
         <div>
           <Route exact path="/" component={Fib} />
           <Route path="/otherpage" component={OtherPage} />
